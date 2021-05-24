1e28a3ca70c99066a69bf3b13dd94fb71af540f0
Shashwat Mishra <shashwat397@gmail.com>
2021-05-25 02:24:44 +0530

    Updates

---
+++ precache-manifest.aa68dbde411028d7d7b12ea99f38d303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ea39c8e1d213cd9ebcb3e1760cf7a4",
+    "url": "/Hulu-app/index.html"
+  },
+  {
+    "revision": "5f20bfbeb32504ed1355",
+    "url": "/Hulu-app/static/css/main.5654c76e.chunk.css"
+  },
+  {
+    "revision": "30914d692b217a08f74a",
+    "url": "/Hulu-app/static/js/2.2482274a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Hulu-app/static/js/2.2482274a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f20bfbeb32504ed1355",
+    "url": "/Hulu-app/static/js/main.eb964521.chunk.js"
+  },
+  {
+    "revision": "c518dddb33c96ba0b3cc",
+    "url": "/Hulu-app/static/js/runtime-main.44a6bd5f.js"
+  }
+]);
